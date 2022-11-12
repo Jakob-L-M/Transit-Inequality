2751868dbf9979221437536055ad4ae142175c20
Jakob-L-M <jakob-151@web.de>
2022-11-12 15:32:49 +0200

    hex representatin

--- map.js
+++ map.js
@@ -23,68 +23,89 @@ function rgbToHex(r, g, b) {
     return "#" + componentToHex(Math.floor(r)) + componentToHex(Math.floor(g)) + componentToHex(Math.floor(b));
 }
 
+var feature_groups = []
+var HEXAGONS = [];
+var TIMEMAP = [];
+var TIME_OF_DAY = '200000'
 
 $.getJSON("scrape/datasets/hsl/hexagons.json", function (hexagons) {
-    console.log("got hexagons", hexagons);
+
+    HEXAGONS = hexagons;
 
     $.getJSON("scrape/datasets/hsl/time_map.json", function (time_map) {
-        console.log("got time map", time_map);
 
-        $.getJSON("scrape/datasets/hsl/090000/hex_1886.json", function (initial) {
+        TIMEMAP = time_map;
 
-            console.log("got initial times", initial);
+        for (t in time_map) {
+            feature_groups.push(new L.FeatureGroup());
+        }
 
-            var feature_groups = []
+        $.getJSON("scrape/datasets/hsl/120000/hex_1746.json", function (initial) {
 
-            for (t in time_map) {
-                feature_groups.push(new L.FeatureGroup());
-            }
+            // console.log("got initial times", initial);
 
-            for (let polygon = 0; polygon < hexagons.length; polygon++) {
-                let current = hexagons[polygon];
-                //console.log(current)
-
-                let time = initial[current.id]
-
-                // hexagon can not be reached
-                if (time == undefined) continue;
-
-                let ind = Math.floor(time / intervall)
-
-                if (time >= 8400) {
-                    ind = 29
-                }
-
-                //console.log(time, time_map[intervall * ind])
-                //console.log(polygons[polygon]['time'], col, rgbToHex(col[0], col[1], col[2]))
-                addPolygon(current.geometry.coordinates[0], ind, time_map[intervall * ind], { name: current.id, time: time }, feature_groups)
-
-            }
-
-            console.log(feature_groups)
-
-            for (feat in feature_groups) {
-                let t = feature_groups[feat]
-                t.timeid = feat;
-
-                t.on('mouseover', function (e) {
-                    t.setStyle({
-                        fillOpacity: 1,
-                        fillColor: '#002561'
-                    })
-                });
-                t.on('mouseout', function (e) {
-                    let col = time_map[intervall * t.timeid]
-                    t.setStyle({
-                        fillOpacity: .8,
-                        fillColor: rgbToHex(col[0], col[1], col[2])
-                    })
-                });
-                t.addTo(map)
-            }
+            setMap('hex_1746');
         });
     });
 })
+
+
+function setMap(hex_id) {
+
+    for (feat in feature_groups) {
+        feature_groups[feat].clearLayers();
+    }
+
+    $.getJSON(`scrape/datasets/hsl/${TIME_OF_DAY}/${hex_id}.json`, function (times) {
+
+        for (let polygon = 0; polygon < HEXAGONS.length; polygon++) {
+            let current = HEXAGONS[polygon];
+            //console.log(current)
+
+            let time = times[current.id]
+
+            // hexagon can not be reached
+            if (time == undefined) continue;
+
+            let ind = Math.floor(time / intervall) + 1
+
+            if (time >= 8400) {
+                ind = 30
+            }
+
+            if (time == -1) {
+                ind = 0
+            }
+
+            //console.log(time, time_map[intervall * ind])
+            //console.log(polygons[polygon]['time'], col, rgbToHex(col[0], col[1], col[2]))
+            addPolygon(current.geometry.coordinates[0], ind, TIMEMAP[ind], { name: current.id, time: time })
+
+        }
+
+        for (feat in feature_groups) {
+            let t = feature_groups[feat]
+            t.timeid = feat;
+
+            t.on('mouseover', function (e) {
+                t.setStyle({
+                    fillOpacity: 1,
+                    weight: 2,
+                })
+            });
+            t.on('mouseout', function (e) {
+                let col = TIMEMAP[t.timeid]
+                let hex = rgbToHex(col[0], col[1], col[2])
+                t.setStyle({
+                    fillOpacity: .85,
+                    fillColor: hex,
+                    weight: 0,
+                })
+            });
+            t.addTo(map)
+        }
+    });
+}
 
 function addCircle(lon, lat, edgecol, col, size, from = "", to = "", time = 0) {
     var circle = L.circle([lat, lon], {
@@ -101,9 +122,8 @@ function addCircle(lon, lat, edgecol, col, size, from = "", to = "", time = 0) {
     circle.addTo(map);
 }
 
-function addPolygon(ponits, ind, col, pol, groups) {
-
-    let poly = L.polygon(ponits, { fillColor: rgbToHex(col[0], col[1], col[2]), weight: 0, color: '#000', fillOpacity: .8 })
-    poly.bindPopup(`Name: ${pol.name}, Time: ${pol.time}`);
-    groups[ind].addLayer(poly)
+function addPolygon(ponits, ind, col, pol) {
+    let poly = L.polygon(ponits, { fillColor: rgbToHex(col[0], col[1], col[2]), weight: 0, color: '#000', fillOpacity: .85 })
+    poly.bindPopup(`Time to get here: ${Math.floor(pol.time / 3600)}:${pol.time % 3600}:${pol.time % 60}h, <button onClick="setMap('${pol.name}')">Set origin here</button>`);
+    feature_groups[ind].addLayer(poly)
 }
